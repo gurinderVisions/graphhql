2cf68fe5b55252e0ad333c5f866c4d96503c4704
visionsIT <sales@visions.net.in>
2020-10-23 15:36:01 +0530

    production

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '@babel/polyfill/noConflict'
+import "@babel/polyfill";  
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import { resolvers, fragmentReplacements } from './resolvers/index'
@@ -20,6 +20,6 @@ const server = new GraphQLServer({
     fragmentReplacements
 })
 
-server.start({ port: process.env.PORT || 4000 }, () => {
+server.start({port: process.env.PORT || 4000  },() => {
     console.log('The server is up!')
 })
